a4969eee85be5ebe1551bcc2b8f9708a801cee3f
Daewoo Kim <hansyo@me.com>
2026-05-28 12:45:24 +0900

    bump(version): update APP_VERSION to v00006 in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import AddTab from './components/AddTab';
 import SearchTab from './components/SearchTab';
 import BottomSheet from './components/BottomSheet';
 
-const APP_VERSION = 'v00005';
+const APP_VERSION = 'v00006';
 
 const AppContent: React.FC = () => {
   const { user, loading: authLoading, authError, myOriginalCode, loginWithGroupCode } = useAuth();
