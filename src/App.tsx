e017f55ddd6a74ab0a4d0eed342b55dd3e87ad2d
Daewoo Kim <hansyo@me.com>
2026-05-28 14:04:41 +0900

    fix(layout): restrict desktop simulator mockup to pure desktop screens to prevent iPad vertical cutoff (v00009)

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import AddTab from './components/AddTab';
 import SearchTab from './components/SearchTab';
 import BottomSheet from './components/BottomSheet';
 
-const APP_VERSION = 'v00008';
+const APP_VERSION = 'v00009';
 
 const AppContent: React.FC = () => {
   const { user, loading: authLoading, authError, myOriginalCode, loginWithGroupCode } = useAuth();
