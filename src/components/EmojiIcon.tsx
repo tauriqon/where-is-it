ae00b06198c550db106d7747e52d6e3fa62398e3
Daewoo Kim <hansyo@me.com>
2026-05-31 17:38:30 +0900

    feat: map newly uploaded space and storage premium icons to emojis, and bump version to v00030

--- src/components/EmojiIcon.tsx
+++ src/components/EmojiIcon.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import potIcon from '../assets/icons/spaces/pot.svg';
+import bedIcon from '../assets/icons/spaces/Bed Emoji.png';
+import sofaIcon from '../assets/icons/spaces/Sofa Doodle.png';
+import bookcaseIcon from '../assets/icons/storages/Bookcase Color Glass.png';
+import buffetIcon from '../assets/icons/storages/Buffet Windows 11 Color.png';
+import closetIcon from '../assets/icons/storages/Closet Office M.png';
 
 interface EmojiIconProps {
   icon: string;
@@ -8,11 +13,94 @@ interface EmojiIconProps {
 }
 
 export const EmojiIcon: React.FC<EmojiIconProps> = ({ icon, size = 24, style }) => {
+  // 1단계 [공간]용 커스텀 아이콘 매퍼
   if (icon === '🍳') {
     return (
       <img 
         src={potIcon} 
         alt="pot" 
+        style={{ 
+          width: `${size}px`, 
+          height: `${size}px`, 
+          display: 'inline-block', 
+          verticalAlign: 'middle',
+          objectFit: 'contain',
+          ...style 
+        }} 
+      />
+    );
+  }
+  if (icon === '🛏️') {
+    return (
+      <img 
+        src={bedIcon} 
+        alt="bed" 
+        style={{ 
+          width: `${size}px`, 
+          height: `${size}px`, 
+          display: 'inline-block', 
+          verticalAlign: 'middle',
+          objectFit: 'contain',
+          ...style 
+        }} 
+      />
+    );
+  }
+  if (icon === '🛋️') {
+    return (
+      <img 
+        src={sofaIcon} 
+        alt="sofa" 
+        style={{ 
+          width: `${size}px`, 
+          height: `${size}px`, 
+          display: 'inline-block', 
+          verticalAlign: 'middle',
+          objectFit: 'contain',
+          ...style 
+        }} 
+      />
+    );
+  }
+
+  // 2단계 [수납처]용 커스텀 아이콘 매퍼
+  if (icon === '📚') {
+    return (
+      <img 
+        src={bookcaseIcon} 
+        alt="bookcase" 
+        style={{ 
+          width: `${size}px`, 
+          height: `${size}px`, 
+          display: 'inline-block', 
+          verticalAlign: 'middle',
+          objectFit: 'contain',
+          ...style 
+        }} 
+      />
+    );
+  }
+  if (icon === '🗄️') {
+    return (
+      <img 
+        src={buffetIcon} 
+        alt="buffet" 
+        style={{ 
+          width: `${size}px`, 
+          height: `${size}px`, 
+          display: 'inline-block', 
+          verticalAlign: 'middle',
+          objectFit: 'contain',
+          ...style 
+        }} 
+      />
+    );
+  }
+  if (icon === '👔') {
+    return (
+      <img 
+        src={closetIcon} 
+        alt="closet" 
         style={{ 
           width: `${size}px`, 
           height: `${size}px`, 
